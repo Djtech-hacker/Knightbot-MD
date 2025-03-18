7a160a3795557dd5a808e45de1c149ae318bc14b
Dj tech <118798191+Djtech-hacker@users.noreply.github.com>
2025-03-18 21:58:25 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Jesus is Lord', // Your name
-  ownerNumber: '2349138458733', //Your number
+  botOwner: 'Evangelist Kcito', // Your name
+  ownerNumber: '2349166215888', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
